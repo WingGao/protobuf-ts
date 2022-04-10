b47b36964526d7495064379e17f041aaf1f58df2
Wing Gao <wing.gao@live.com>
2022-04-10 15:33:27 +0800

    feat: 默认可选

--- packages/plugin/src/interpreter_wing.ts
+++ packages/plugin/src/interpreter_wing.ts
@@ -4,6 +4,7 @@ function WingPatchType(type:IMessageType<any>){
     type.fields.forEach(field=>{
         // 替换为大写
         field.localName = field.jsonName
+        field.opt = true //默认都是可选
     })
     return type
 }
